9d2ea9dcc06fe3e4d72773ea99638e258df48391
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2019-06-26 01:09:55 -0700

    Updated discord token application. Added command placeholders and created filter command to remove common words listed in the array. Added another RegEx entry to clean up output.

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const TOKEN = "TOKENGOESHERE";
+var logger = require('winston');
+var auth = require('./auth.json');
+
+//Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {colorize: true});
+logger.level = 'debug';
+
+//Initialize Discord bot
+const client = new Discord.Client({
+	token: auth.token
+});
+
 
 client.on('message', (message)=>{
-  if(!message.author.bot){
-    filteredmessage = message.content
-    //message.channel.send("It\'s fine now. Why? Because I am here!");
-    //we want to read the message here
-    //we can use the message.content to get the message content
-    //have to filter through the message content to get rid of words like "like, and, the etc"
 
+	//Filter message command
+	if(!message.author.bot && message.content.startsWith("!filter")){
+	    filteredmessage = message.content
+	    //message.channel.send("It\'s fine now. Why? Because I am here!");
+	    //we want to read the message here
+	    //we can use the message.content to get the message content
+	    //have to filter through the message content to get rid of words like "like, and, the etc"
 
-    filteredmessage = removeUselessWords(filteredmessage);
-    if(filteredmessage != " "){
-      console.log(filteredmessage);
-      message.channel.send(filteredmessage);
-    }
-    else{
-      message.channel.send("No usable words nerd");
-    }
-  }
-});
+	    filteredmessage = removeUselessWords(filteredmessage);
+	    if(filteredmessage != " "){
+	      console.log(filteredmessage);
+	      message.channel.send(filteredmessage);
+	    }
+		else{
+	      message.channel.send("No usable words nerd");
+	    }
+	  }
+  	//Pirate translator
+	else if(!message.author.bot && message.content.startsWith("!pirate")){
+	  	console.log("Executed !pirate command");
+	  	message.channel.send("Executed !pirate command");
+	  }
+  	//Shakespeare english tranlsator
+	else if(!message.author.bot && message.content.startsWith("!")){
+	  	console.log("Executed !peary command");
+	  	message.channel.send("Executed !peary command");
+	  }
+  	//Corporate BS generator
+	else if(!message.author.bot && message.content.startsWith("!corpbs")){
+	  	console.log("Executed !corpbs command");
+	  	message.channel.send("Executed !corpbs command");
+	  }
+
+	});
 
     var removeUselessWords = function(txt) {
       var uselessWordsArray =
@@ -29,10 +57,18 @@ client.on('message', (message)=>{
           "do", "does", "dare", "dared", "how", "havent", "i", "in", "is", "many", "may", "might", "much", "must", "of",
           "on", "or", "should", "shouldnt", "so", "such", "the",
           "them", "they", "to", "us",  "we", "what", "who", "why",
-          "with", "wont", "would", "wouldnt", "you"
+          "with", "wont", "would", "wouldnt", "you", "and|"
         ];
         var expStr = uselessWordsArray.join("|");
-        return txt.replace(new RegExp('\\b(' + expStr + ')\\b', 'gi'), ' ')
+		// logger.log({
+		//   level: 'debug',
+		//   message: expStr
+		// });
+		//Removes command text
+        var text = txt.replace(new RegExp('\!filter', 'gi'), ' ')
+                        .replace(/\s{2,}/g, ' ');     
+        //Removes useless words         
+        return text.replace(new RegExp('\\b(' + expStr + ')\\b', 'gi'), ' ')
                         .replace(/\s{2,}/g, ' ');
     }
-client.login(TOKEN);
+client.login(auth.token);
